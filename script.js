7ed61d9b873bc5deea2de9811d86d23ee98adc29
AtrixV <111180274+AtrixV@users.noreply.github.com>
2024-07-28 01:41:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   const url = process.env.WEBSITE_URL; // Получаем URL из переменных окружения
+  const duration = process.env.DURSTION;
   if (!url) {
     console.error('No URL provided');
     process.exit(1);
@@ -13,7 +14,7 @@ const puppeteer = require('puppeteer');
   await page.goto(url);
 
   // Ожидание 5 секунд
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(duration*1000);
 
   // Закрытие браузера
   await browser.close();
